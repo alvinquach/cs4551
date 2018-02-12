02fd30f7c75c8f692bc894c8cf2cde48532215ae
Unknown <alvinquach91@gmail.com>
2018-02-12 12:34:33 -0800

    added ClonableImage class

---
+++ Homework 1/src/com/alvinquach/cs4551/homework1/application/CS4551_Quach.java
@@ -0,0 +1,9 @@
+package com.alvinquach.cs4551.homework1.application;
+
+public class CS4551_Quach {
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ Homework 1/src/com/alvinquach/cs4551/homework1/image/ClonableImage.java
@@ -0,0 +1,33 @@
+package com.alvinquach.cs4551.homework1.image;
+
+/**
+ * Extension of the {@code Image} class that provides cloning capabilities.
+ * @author Alvin Quach
+ */
+public class ClonableImage extends Image implements Cloneable {
+
+	public ClonableImage(int w, int h) {
+		super(w, h);
+	}
+
+	public ClonableImage(String fn) {
+		super(fn);
+	}
+
+	/** Set pixel color by sRGB value. */
+	public void setPixel(int x, int y, int rgb) {
+		img.setRGB(x, y, rgb);
+	}
+
+	/** Returns a clone of this Image. */
+	public ClonableImage clone() {
+		ClonableImage newImage = new ClonableImage(img.getWidth(), img.getHeight());
+		for (int x = 0; x < img.getWidth(); x++) {
+			for (int y = 0; y < img.getHeight(); y++) {
+				newImage.setPixel(x, y, img.getRGB(x, y));
+			}
+		}
+		return newImage;
+	}
+
+}
